dc13a8bb4aefe722c7907a7c7db8f21267047831
Sami Javaid <sami.javaid73@gmail.com>
2018-04-11 17:34:33 +0500

    Javascript Challenge
    a small quiz made with the help of javascript basics

---
+++ question.js
@@ -0,0 +1,37 @@
+var questions =[{
+    "question": "Inside which HTML element do we put the JavaScript?",
+    "option1":"<js>",
+    "option2":"<javascript>",
+    "option3":"<scripting>",
+    "option4":"<script>",
+    "answer":"4"
+                },
+                {
+    "question": "Where is the correct place to insert a JavaScript?",
+    "option1":"The <body> section",
+    "option2":"The <head> section",
+    "option3":"Both the <head> section and the <body> section are correct",
+    "option4":"none of the above",
+    "answer":"3"
+},{
+    "question": "How do you write 'Hello World' in an alert box?",
+    "option1":"alertBox('Hello World');",
+    "option2":"alert('Hello World');",
+    "option3":"msg('Hello World');",
+    "option4":"msgBox('Hello World');",
+    "answer":"2"
+},{
+    "question": "How do you create a function in JavaScript?",
+    "option1":"function:myFunction()",
+    "option2":"function = myFunction()",
+    "option3":"function myFunction()",
+    "option4":"none of the above",
+    "answer":"3"
+},{
+    "question": "How do you call a function named 'myFunction'?",
+    "option1":"call function myFunction()",
+    "option2":"call myFunction()",
+    "option3":"myFunction()",
+    "option4":"none of the above",
+    "answer":"3"
+}]
